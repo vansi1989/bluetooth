76be3775a8026485903c1ee509fa2e54912ec0f2
Fan_office <327918578@qq.com>
2016-08-25 15:48:06 +0800

    修改作者名

--- src/com/fan/bluetooth/MainActivity.java
+++ src/com/fan/bluetooth/MainActivity.java
@@ -1,24 +1,37 @@
 package com.fan.bluetooth;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.UUID;
+
+import android.R.layout;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	private BluetoothAdapter mBluetoothAdapter;
 	private BluetoothReceiver mBluetoothReceiver;
+	private LinearLayout ll_containner;
+	private BluetoothSocket socket;
+	private OutputStream os;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		init();
 		//获取蓝牙管理器
 		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 		mBluetoothAdapter.enable();
@@ -39,11 +52,32 @@ public class MainActivity extends Activity {
 		public void onReceive(Context context, Intent intent) {
 			String action = intent.getAction();		
 			if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
-				Toast.makeText(context, "开始扫描蓝牙设备", 0).show();
+				Toast.makeText(context, "开始扫描蓝牙设备", Toast.LENGTH_SHORT).show();
 			} else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
-				Toast.makeText(context, "扫描蓝牙设备完成", 0).show();
+				Toast.makeText(context, "扫描蓝牙设备完成", Toast.LENGTH_SHORT).show();
 			} else if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-				Toast.makeText(context, "发现了蓝牙设备", 0).show();
+				System.out.println("发现了蓝牙设备");
+				//get the bluetooth device object from the intent
+				final BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+//				System.out.println((device.getName() + "\n" + device.getAddress()));
+				TextView tv = new TextView(MainActivity.this);
+				tv.setText((device.getName() + "--" + device.getAddress()));
+				tv.setTextSize(25);
+				tv.setTag(device);
+				tv.setOnClickListener(new View.OnClickListener() {
+					public void onClick(View v) {
+						try {
+							socket = device.createInsecureRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));
+//							socket = device.createInsecureRfcommSocketToServiceRecord(UUID.randomUUID());
+							socket.connect();
+							os = socket.getOutputStream();
+							Toast.makeText(MainActivity.this, "成功连接"+device.getName(), Toast.LENGTH_SHORT).show();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
+				});
+				ll_containner.addView(tv);
 			} 
 		}
 	}
@@ -54,7 +88,28 @@ public class MainActivity extends Activity {
 		mBluetoothReceiver = null;
 		super.onDestroy();
 	}
-	private void scan(View view) {
+	public void scan(View view) {
 		mBluetoothAdapter.startDiscovery();
+		ll_containner.clearDisappearingChildren();
+	}
+	public void stop(View view) {
+		mBluetoothAdapter.cancelDiscovery();
+	}
+	public void switchLight(View view) {
+		return;
+	}
+	public void connect(View view) {
+		try {
+			os.write(0x33);
+			os.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void init() {
+		ll_containner = (LinearLayout) findViewById(R.id.ll_containner);
 	}
 }
